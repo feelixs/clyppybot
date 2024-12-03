a60d411825d70a99efe5bf111ca3bbc976f9e47c
Michael Felix <mikemh@uri.edu>
2024-12-03 14:04:48 -0500

    twitch

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ Bot.twitch = t
 
 async def main():
     Bot.load_extension('cogs.base')
-    Bot.load_extension('cogs.autoembed')
+    Bot.load_extension('cogs.twitch_autoembed')
     await Bot.astart(token=os.getenv('CLYPP_TOKEN'))
 
 
