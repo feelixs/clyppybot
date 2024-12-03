b1260963c4a85e644d674b76d303c39a2e099747
Michael Felix <mikemh@uri.edu>
2024-12-03 12:35:48 -0500

    Update clip.py

--- bot/twitch/clip.py
+++ bot/twitch/clip.py
@@ -10,6 +10,8 @@ import asyncio
 import aiohttp
 import logging
 import os
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 
 
 class Clip:
@@ -43,7 +45,7 @@ class Clip:
         def sync_get_link(query_href_text):
             options = uc.ChromeOptions()
             options.arguments.extend(["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"])  # << this
-            driver = uc.Chrome(options=options)
+            driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
             try:
                 driver.get(query_href_text)
                 element = driver.find_element(By.CSS_SELECTOR, f"video[src*='{text}']")
