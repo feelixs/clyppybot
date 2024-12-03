9a4a9cb02be202be290d708916be9adc368e1fa8
Michael Felix <mikemh@uri.edu>
2024-12-03 16:10:10 -0500

    Update misc.py

--- bot/kick/misc.py
+++ bot/kick/misc.py
@@ -9,14 +9,15 @@ class KickMisc:
         self.logger = logging.getLogger(__name__)
 
     @staticmethod
-    def parse_clip_url(url: str) -> str:
+    def parse_clip_url(url: str) -> (str, str):
+        user = url.split('kick.com/')[-1].split("/")[0]
         if url.endswith("/"):
             url = url[:-1]  # remove trailing slash
         slug = str(url).split('/')[-1]
         if "?" in slug:
             slug = slug.split('?')[0]
         slug = slug.replace("clip_", "")
-        return slug
+        return slug, user
 
     @staticmethod
     def is_kick_clip_link(url: str) -> bool:
@@ -27,5 +28,5 @@ class KickMisc:
         return bool(re.match(kick_clip_pattern, url))
 
     async def get_clip(self, url: str) -> KickClip:
-        slug = self.parse_clip_url(url)
-        return KickClip(slug)
+        slug, user = self.parse_clip_url(url)
+        return KickClip(slug, user)
