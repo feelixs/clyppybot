6c29161e05e562a592b81f6b390609cec8f62d21
Michael Felix <mikemh@uri.edu>
2024-12-04 17:23:50 -0500

    Update base.py

--- cogs/base.py
+++ cogs/base.py
@@ -33,6 +33,10 @@ class Base(Extension):
             "CLYPPY supports uploading Twitch clips in Full HD to your Discord channels! Send a valid Twitch Clip link to get started.\n\n"
             "**TROUBLESHOOTING**\nIf CLYPPY isn't responding to your Twitch Clip links, it could be because it has incorrect permissions for your Discord channel."
             " Required permissions are: `Attach Files`, `Send Messages`\n\n"
+            "**RECOMMENDATIONS**\n "
+            " - So that CLYPPY can respond correctly, please ensure he has the permission listed above.\n"
+            " - Discord sometimes adds a preview for specific clip links. To reduce clutter, you can disable this feature for yourself in your Discord settings under:"
+            "\n**App Settings > Chat > Display Images, Videos, and LOLCats > When posted as links to chat** - uncheck this settings.\n\n"
             "**UPDATE Dec 3rd 2024** CLYPPY is back online after a break. We are working on improving the service and adding new features. Stay tuned!")
         help_embed = Embed(title="About CLYPPY", description=about)
         help_embed.description += create_nexus_str()
