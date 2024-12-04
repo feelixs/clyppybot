0a19647971c7133c82dd596c92b1a9b047a9b785
Michael Felix <mikemh@uri.edu>
2024-12-03 20:13:00 -0500

    conditional reply for kick

--- bot/tools/__init__.py
+++ bot/tools/__init__.py
@@ -1,1 +1,1 @@
-from .misc import create_nexus_str
+from .misc import create_nexus_str, reply_if_loud
--- cogs/kickautoembed.py
+++ cogs/kickautoembed.py
@@ -3,7 +3,7 @@ import interactions.api.events
 from interactions import Extension, Message, Embed, Permissions, listen
 from interactions.api.events import MessageCreate
 from bot.kick import KickClip
-from bot.tools import create_nexus_str
+from bot.tools import create_nexus_str, reply_if_loud
 from typing import Union, List
 import asyncio
 import os
@@ -92,7 +92,7 @@ class KickAutoEmbed(Extension):
                                     f"Try linking a shorter clip!\n"
                                     "This clip file was previously reported as too large to fit Discord's limit.")
             emb.description += create_nexus_str()
-            await respond_to.reply(embed=emb)
+            await reply_if_loud(self.bot, respond_to, embed=emb)
             self.logger.info(
                 f"Skipping quick embed for clip {parsed_id} in {respond_to.guild.name} - {respond_to.channel.name}, clip was previously reported too large")
             return 1
@@ -102,7 +102,7 @@ class KickAutoEmbed(Extension):
             emb = Embed(title="**Invalid Clip Link**",
                         description=f"Looks like the Kick clip `{clip_link}` couldn't be downloaded. Verify that it exists")
             emb.description += create_nexus_str()
-            await respond_to.reply(embed=emb)
+            await reply_if_loud(self.bot, respond_to, embed=emb)
             return 1
         clip_file = await self._dl.download_clip(clip, root_msg=respond_to)
         if clip_file is None:
@@ -112,7 +112,7 @@ class KickAutoEmbed(Extension):
                                             {clip_link}\nPlease try linking it again.\n"
                                     "If the issue keeps on happening, please contact us on our support server.")
             emb.description += create_nexus_str()
-            await respond_to.reply(embed=emb, delete_after=60)
+            await reply_if_loud(self.bot, respond_to, embed=emb, delete_after=60)
             return 1
         try:
             if include_link:
@@ -128,7 +128,7 @@ class KickAutoEmbed(Extension):
                 emb.description += create_nexus_str()
                 self.too_large_clips.append(clip.id)
                 self.logger.info(f"Clip {clip.id} was too large to embed in {respond_to.guild.name} - {respond_to.channel.name}")
-                await respond_to.reply(embed=emb)
+                await reply_if_loud(self.bot, respond_to, embed=emb)
                 return 1
             else:
                 self.logger.info(f"Unknown HTTPException in _process_this_clip_link: {traceback.format_exc()}")
@@ -137,7 +137,7 @@ class KickAutoEmbed(Extension):
                                         f"\n\nPlease try linking it again.\n"
                                         "If the issue keeps on happening, please contact us on our support server.")
                 emb.description += create_nexus_str()
-                await respond_to.reply(embed=emb, delete_after=60)
+                await reply_if_loud(self.bot, respond_to, embed=emb, delete_after=60)
                 return 1
         except:
             self.logger.info(f"Unknown Exception in _process_this_clip_link: {traceback.format_exc()}")
@@ -146,5 +146,5 @@ class KickAutoEmbed(Extension):
                                     f"\n\nPlease try linking it again.\n"
                                     "If the issue keeps on happening, please contact us on our support server.")
             emb.description += create_nexus_str()
-            await respond_to.reply(embed=emb, delete_after=60)
+            await reply_if_loud(self.bot, respond_to, embed=emb, delete_after=60)
             return 1
