9221ab679695c418e60aa57fb3f9ab27429f3a84
Michael Felix <mikemh@uri.edu>
2024-12-03 21:55:25 -0500

    Update kickautoembed.py

--- cogs/kickautoembed.py
+++ cogs/kickautoembed.py
@@ -40,7 +40,7 @@ class KickAutoEmbed(Extension):
                 size_mb = os.path.getsize(f) / (1024 * 1024)
                 if size_mb > 25:
                     # Get guild setting for handling large files
-                    too_large_setting = await self._parent.bot.get_too_large(root_msg.guild.id)
+                    too_large_setting = await self._parent.bot.guild_settings_get_too_large(root_msg.guild.id)
 
                     if too_large_setting == "trim":
                         # Calculate target duration and trim
