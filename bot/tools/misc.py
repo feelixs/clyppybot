dcc67c4fa50437b5be26172021f3e47c3208e912
Michael Felix <mikemh@uri.edu>
2024-12-05 13:07:57 -0500

    Revert "Update misc.py"
    This reverts commit 39a22b0424fde460c314762013f735b0658f347a.

--- bot/tools/misc.py
+++ bot/tools/misc.py
@@ -51,7 +51,7 @@ class DownloadManager:
             size_mb = os.path.getsize(f) / (1024 * 1024)
             if size_mb > 25:
                 # Get guild setting for handling large files
-                too_large_setting = self._parent.bot.guild_settings.get_too_large(guild_ctx.id).setting_str
+                too_large_setting = self._parent.bot.guild_settings.get_too_large(guild_ctx.id)
 
                 if too_large_setting == "trim":
                     # Calculate target duration and trim
