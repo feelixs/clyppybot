f87aab3450d2fc07455445132e312b20c93f908d
Michael Felix <mikemh@uri.edu>
2025-01-03 22:08:46 -0500

    Update misc.py

--- bot/tools/misc.py
+++ bot/tools/misc.py
@@ -176,8 +176,7 @@ class Tools:
 
     async def send_error_message(self, msg_embed, dm_content, guild, ctx, bot, delete_after_on_reply=None):
         err = ""
-        self.logger.info(f"{ctx.author.id}, {guild.id}")
-        if guild is None:
+        if guild.id == ctx.author.id:
             pass  # don't use 'dm' setting if we're already in a dm, just reply
         elif bot.guild_settings.is_dm_on_error(guild.id):
             await self.send_dm_err_msg(ctx, guild, dm_content)
