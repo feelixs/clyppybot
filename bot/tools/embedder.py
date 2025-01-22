be2b3f14847bce5a5deb77ebe291fa7e4533b6fa
Michael Felix <mikemh@uri.edu>
2025-01-22 16:08:10 -0500

    Update embedder.py

--- bot/tools/embedder.py
+++ bot/tools/embedder.py
@@ -240,7 +240,10 @@ class AutoEmbedder:
                     await respond_to.reply(clip.clyppy_url, components=comp)
 
                 now_utc = datetime.now(tz=timezone.utc).timestamp()
-                respond_to_utc = respond_to.timestamp.astimezone(tz=timezone.utc).timestamp()
+                if isinstance(respond_to, SlashContext):
+                    respond_to_utc = respond_to.message.timestamp.astimezone(tz=timezone.utc).timestamp()
+                else:
+                    respond_to_utc = respond_to.timestamp.astimezone(tz=timezone.utc).timestamp()
                 my_response_time = round((now_utc - respond_to_utc), 2)
                 self.logger.info(f"Successfully embedded clip {clip.id} in {guild.name} - #{chn} in {my_response_time} seconds")
                 if result['success']:
