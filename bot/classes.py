07a5143c8a9c169809ea8ead5117a5c53b2297bb
Michael Felix <mikemh@uri.edu>
2025-02-01 13:38:04 -0500

    Update classes.py

--- bot/classes.py
+++ bot/classes.py
@@ -204,7 +204,7 @@ class BaseClip(ABC):
                     remote_url=format_info['url'],
                     local_file_path=None,
                     duration=duration,
-                    filesize=0,  # since yt-dlp was rigged to return an url, video is hosted on another cdn
+                    filesize=info.get('filesize', 0),
                     width=format_info['width'],
                     height=format_info['height'],
                     video_name=title
@@ -240,7 +240,7 @@ class BaseClip(ABC):
                         remote_url=format_info['url'],
                         local_file_path=None,
                         duration=duration,
-                        filesize=0,
+                        filesize=best_format.get('filesize', 0),
                         width=format_info['width'],
                         height=format_info['height'],
                         video_name=title
