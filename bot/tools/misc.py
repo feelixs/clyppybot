a67869bb3fe6deadd992dabad7b54392a024e1bb
Michael Felix <myellahouse@gmail.com>
2025-02-10 11:46:40 -0500

    Update misc.py

--- bot/tools/misc.py
+++ bot/tools/misc.py
@@ -61,7 +61,7 @@ class DownloadManager:
             res = await clip.overwrite_mp4(new.remote_url)
             if res['code'] == 202:
                 self._parent.logger.info(f"https://clyppy.io/{clip.clyppy_id} does not exist, so no overwrite was performed")
-                
+
             r.filesize = new.filesize
             r.remote_url = new.remote_url
         elif overwrite_on_server and (r.can_be_uploaded and can_send_files):
