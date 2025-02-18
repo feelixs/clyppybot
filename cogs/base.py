0dc6cc94e10701a63958ff211159ec9ec2c12918
Michael Felix <myellahouse@gmail.com>
2025-02-17 22:48:05 -0500

    Update base.py

--- cogs/base.py
+++ cogs/base.py
@@ -17,7 +17,7 @@ import time
 
 LOGGER_WEBHOOK = os.getenv('LOG_WEBHOOK')
 
-VERSION = "1.5.2b"
+VERSION = "1.5.3b"
 
 
 def compute_platform(url: str, bot) -> Tuple[Optional[BaseMisc], Optional[str]]:
