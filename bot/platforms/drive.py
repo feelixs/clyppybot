9917c03812d4ec13eb658286c0f460ecc982be26
Michael Felix <myellahouse@gmail.com>
2025-02-24 13:23:05 -0500

    Update drive.py

--- bot/platforms/drive.py
+++ bot/platforms/drive.py
@@ -7,6 +7,7 @@ class GoogleDriveMisc(BaseMisc):
     def __init__(self):
         super().__init__()
         self.platform_name = "Google Drive"
+        self.dl_timeout_secs = 120
 
     def parse_clip_url(self, url: str, extended_url_formats=False) -> Optional[str]:
         """
