c673299328c8118a1801179b39a4b956ca5d7214
Michael Felix <myellahouse@gmail.com>
2025-02-26 10:26:06 -0500

    Update reddit.py

--- bot/platforms/reddit.py
+++ bot/platforms/reddit.py
@@ -38,7 +38,7 @@ class RedditMisc(BaseMisc):
             r'(?:https?://)?(?:www\.)?reddit\.com/gallery/([a-zA-Z0-9]+)',  # Gallery links
             r'(?:https?://)?(?:www\.)?reddit\.com/user/[^/]+/comments/([a-zA-Z0-9]+)',  # User posts
             r'(?:https?://)?(?:www\.)?reddit\.com/r/[^/]+/duplicates/([a-zA-Z0-9]+)',  # Crossposts
-            r'(?:https?://)?(?:www\.)?reddit\.com/r/[^/]+/s/([a-zA-Z0-9]+)'  # Share links
+            r'(?:https?://)?(?:www\.)?reddit\.com/r/[^/]+/s/([a-zA-Z0-9]+)',  # Share links
             r'(?:https?://)?v\.redd\.it/([a-zA-Z0-9]+)'  # Video links
         ]
 
