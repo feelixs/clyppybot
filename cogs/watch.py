df2b9d42a6a38b372dc864b3070f350436f5bf96
Michael Felix <myellahouse@gmail.com>
2025-02-27 17:49:24 -0500

    Update watch.py

--- cogs/watch.py
+++ cogs/watch.py
@@ -1,8 +1,12 @@
 from interactions import Extension, listen
 from interactions.api.events import MessageCreate
 import logging
-import os
-from datetime import datetime, timedelta
+import re
+
+
+CLYPPY_SUPPORT_SERVER_ID = 1117149574730104872
+CLYPPY_VOTE_ROLE = 1337067081941647472
+VOTE_WEBHOOK_USERID = 1337076281040179240
 
 
 class Watch(Extension):
@@ -10,12 +14,47 @@ class Watch(Extension):
         self.bot = bot
         self.logger = logging.getLogger(__name__)
 
+    async def give_votes_roles(self, userid, total_votes: int):
+        guild = self.bot.get_guild(CLYPPY_SUPPORT_SERVER_ID)
+        try:
+            member = guild.get_member(userid)
+            self.logger.info(f"Giving vote roles to {member.username}")
+            voter_role = guild.get_role(CLYPPY_VOTE_ROLE)
+        except Exception as e:
+            self.logger.info(f"Error getting member or role: {e}")
+            return
+
+        the_role, old_roles = None, []
+        for r in guild.roles:
+            if match := re.match(r'(\d+) Votes?', r.name):
+                role_votes = int(match.group(1))
+                if role_votes == total_votes:
+                    self.logger.info(f"Setting role {r.name} because it has {total_votes} votes")
+                    the_role = r
+                elif role_votes < total_votes:
+                    self.logger.info(f"Removing role {r.name} because it has less votes than {total_votes}")
+                    old_roles.append(r)
+
+        if the_role is None:
+            self.logger.info(f"Creating role `{total_votes} Votes` because it doesn't exist")
+            the_role = await guild.create_role(name=f"{total_votes} Votes")
+
+        await member.remove_roles(old_roles)
+        await member.add_roles([voter_role, the_role])
+
     @listen(MessageCreate)
     async def on_message_create(self, event):
-        try:
-            if "clyppy" in event.message.content or '1111723928604381314' in event.message.content:
-                self.logger.info(f"{event.message.guild.name}: #{event.message.channel.name} "
-                                 f"@{event.message.author.username} - \"{event.message.content}\"")
-        except:
-            # in dms it wont work
-            pass
+        if event.message.guild is None:
+            return  # in dms it won't work
+
+        if "clyppy" in event.message.content or '1111723928604381314' in event.message.content:
+            self.logger.info(f"{event.message.guild.name}: #{event.message.channel.name} "
+                             f"@{event.message.author.username} - \"{event.message.content}\"")
+
+            if event.message.author.id == VOTE_WEBHOOK_USERID:
+                pattern = r"<@(\d+)> just gave \(1\) vote\(s\) for <@\d+> on \[[^\]]+\]\([^)]+\) and earned \d+ VIP tokens! They now have (\d+) votes in total!"
+                match = re.match(pattern, event.message.content)
+                if match:
+                    userid = match.group(1)
+                    vote_total = match.group(2)
+                    await self.give_votes_roles(userid, vote_total)
