124d6e9b366a0a8e2ae944f6211377db0b0e1b66
Michael Felix <myellahouse@gmail.com>
2025-03-01 12:34:10 -0500

    d

--- bot/tools/__init__.py
+++ bot/tools/__init__.py
@@ -1,4 +1,3 @@
 from .misc import (create_nexus_str, Tools, DownloadManager, POSSIBLE_ON_ERRORS, POSSIBLE_TOO_LARGE,
                    POSSIBLE_EMBED_BUTTONS, GuildType)
 from .embedder import AutoEmbedder
-from .io import is_404, get_aiohttp_session
