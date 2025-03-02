897e4f66e2569790842d441002c4b11741c36bf3
Michael Felix <myellahouse@gmail.com>
2025-03-02 11:54:25 -0500

    Update embedder.py

--- bot/tools/embedder.py
+++ bot/tools/embedder.py
@@ -92,9 +92,10 @@ class AutoEmbedder:
                     if isinstance(event.message.channel, TYPE_THREAD_CHANNEL):
                         return 1
 
-                if (event.message.channel.type == GuildPrivateThread or event.message.channel.type == GuildPublicThread) and self.platform_tools.is_nsfw:
-                    # GuildPublicThread has no attribute nsfw
-                    return 1
+                if event.message.channel.type == GuildPrivateThread or event.message.channel.type == GuildPublicThread:
+                    if self.platform_tools.is_nsfw:
+                        # GuildPublicThread has no attribute nsfw
+                        return 1
                 elif not event.message.channel.nsfw and self.platform_tools.is_nsfw:
                     # only allow nsfw in nsfw channels
                     return 1
