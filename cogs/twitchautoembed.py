1dc68f3600510762f1b065bcd74145249cac31ce
Michael Felix <mikemh@uri.edu>
2025-03-04 16:00:17 -0500

    Update twitchautoembed.py

--- cogs/twitchautoembed.py
+++ cogs/twitchautoembed.py
@@ -1,7 +1,5 @@
 from bot.classes import BaseAutoEmbed
 
-# We'll directly use the BaseAutoEmbed class since it already has all the functionality
-# we need, and just needs to be initialized with the right platform and always_embed flag
 
 def setup(bot):
     # Create a BaseAutoEmbed instance with the Twitch platform
