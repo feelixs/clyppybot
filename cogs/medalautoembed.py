af4a9ccc2b63a75f479432f1244b3eae1d8f9dc0
Michael Felix <mikemh@uri.edu>
2025-03-04 13:46:08 -0500

    !embed command

--- cogs/medalautoembed.py
+++ cogs/medalautoembed.py
@@ -1,14 +1,18 @@
 from interactions import Extension, listen
 from interactions.api.events import MessageCreate
 from bot.tools.embedder import AutoEmbedder
+from bot.env import EMBED_TXT_COMMAND
 import logging
 
 
 class MedalAutoEmbed(Extension):
     def __init__(self, bot):
-        self.embedder = AutoEmbedder(bot, bot.medal, logging.getLogger(__name__))
+        self.platform = self.bot.medal
+        self.embedder = AutoEmbedder(bot, self.platform, logging.getLogger(__name__))
 
     @listen(MessageCreate)
     async def on_message_create(self, event):
-        if self.bot.medal.is_dl_server(event.message.guild):
-            await self.embedder.on_message_create(event)
+        message_is_embed_command = (event.message.content.startswith(f"{EMBED_TXT_COMMAND} ")  # support text command (!embed url)
+                                   and self.platform.is_clip_link(event.message.content.split(" ")[-1]))
+        if self.platform.is_dl_server(event.message.guild) or message_is_embed_command:
+            await self.embedder.on_message_create(event, is_embed_text_command=message_is_embed_command)
