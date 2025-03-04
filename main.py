b720570dc12f41aa37944472d5b887264cd0d2d9
Michael Felix <mikemh@uri.edu>
2025-03-04 18:59:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ class BaseAutoEmbedForConsistency(BaseAutoEmbed):
     def __init__(self, bot):
         self.bot = bot
         self.logger = logging.getLogger(self.__class__.__name__)
-        self.platform = None
         super().__init__(self)
+        self.platform = None
 
 
 async def save_to_server():
