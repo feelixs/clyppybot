5da6684ac48492985f58decd1060e0b5fc969acc
Michael Felix <mikemh@uri.edu>
2025-03-04 15:16:55 -0500

    Update base.py

--- cogs/base.py
+++ cogs/base.py
@@ -1,14 +1,61 @@
+from bot.classes import BaseMisc
+from bot.errors import VideoTooLong, NoDuration, ClipFailure, NoPermsToView
 from interactions import (Extension, Embed, slash_command, SlashContext, SlashCommandOption, OptionType, listen,
-    Permissions, ActivityType, Activity, Task, IntervalTrigger, ComponentContext, component_callback)
+    Permissions, ActivityType, Activity, Task, IntervalTrigger, ComponentContext, component_callback, TYPE_THREAD_CHANNEL)
 from bot.tools.misc import SUPPORT_SERVER_URL, TOPGG_VOTE_LINK, create_nexus_str
-from bot.env import POSSIBLE_ON_ERRORS, POSSIBLE_EMBED_BUTTONS, INFINITY_VOTE_LINK, APPUSE_LOG_WEBHOOK, VERSION, DLIST_VOTE_LINK, EMBED_W_TOKEN_MAX_LEN
+from bot.env import POSSIBLE_ON_ERRORS, POSSIBLE_EMBED_BUTTONS, INFINITY_VOTE_LINK, LOGGER_WEBHOOK, APPUSE_LOG_WEBHOOK, \
+    VERSION, DLIST_VOTE_LINK, MAX_VIDEO_LEN_SEC, EMBED_TOKEN_COST, EMBED_W_TOKEN_MAX_LEN
 from interactions.api.events.discord import GuildJoin, GuildLeft
-from bot.types import COLOR_GREEN, COLOR_RED
+from bot.tools.embedder import AutoEmbedder
 from bot.io import get_aiohttp_session
+from bot.types import GuildType, COLOR_GREEN, COLOR_RED
+from typing import Tuple, Optional
 from re import compile
+import asyncio
 import logging
 import aiohttp
+import time
 import os
+
+
+def compute_platform(url: str, bot) -> Tuple[Optional[BaseMisc], Optional[str]]:
+    """Determine the platform and clip ID from the URL"""
+    for this_platform in bot.platform_list:
+        if slug := this_platform.parse_clip_url(url):
+            return this_platform, slug
+
+    return None, None
+
+
+async def send_webhook(title: str, load: str, color=None, url=None, in_test=False):
+    if not in_test and os.getenv("TEST"):
+        return
+
+    if url is None:
+        url = LOGGER_WEBHOOK
+
+    # Create a rich embed
+    if color is None:
+        color = 5814783  # Blue color
+    payload = {
+        "embeds": [{
+            "title": title,
+            "description": load,
+            "color": color,
+        }]
+    }
+
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.post(url, json=payload) as response:
+                if response.status == 204:
+                    print(f"Successfully sent logger webhook: {load}")
+                else:
+                    print(f"Failed to send logger webhook. Status: {response.status}")
+                return response.status
+        except Exception as e:
+            print(f"Error sending log webhook: {str(e)}")
+            return None
 
 
 class Base(Extension):
@@ -17,6 +64,33 @@ class Base(Extension):
         self.ready = False
         self.logger = logging.getLogger(__name__)
         self.save_task = Task(self.db_save_task, IntervalTrigger(seconds=60 * 30))  # save db every 30 minutes
+        self.currently_downloading_for_embed = []
+        self.currently_embedding_users = []
+
+    @staticmethod
+    async def _fetch_tokens(user):
+        url = 'https://clyppy.io/api/tokens/get/'
+        headers = {
+            'X-API-Key': os.getenv('clyppy_post_key'),
+            'Content-Type': 'application/json'
+        }
+        j = {'userid': user.id, 'username': user.username}
+        async with get_aiohttp_session() as session:
+            async with session.get(url, json=j, headers=headers) as response:
+                if response.status == 200:
+                    j = await response.json()
+                    return j['tokens']
+                else:
+                    error_data = await response.json()
+                    raise Exception(f"Failed to fetch user's VIP tokens: {error_data.get('error', 'Unknown error')}")
+
+    @staticmethod
+    async def _handle_timeout(ctx: SlashContext, url: str, amt: int):
+        """Handle timeout for embed processing"""
+        await asyncio.sleep(amt)
+        if not ctx.responded:
+            await ctx.send(f"An error occurred with your input `{url}` {create_nexus_str()}")
+            raise TimeoutError(f"Waiting for clip {url} download timed out")
 
     @staticmethod
     async def get_clip_info(clip_id: str):
@@ -124,7 +198,7 @@ class Base(Extension):
     @slash_command(name="tokens", description="View your VIP tokens!")
     async def tokens(self, ctx: SlashContext):
         await ctx.defer()
-        tokens = await self.bot.platform_tools.fetch_tokens(ctx.user)
+        tokens = await self._fetch_tokens(ctx.user)
         await ctx.send(f"You have `{tokens}` VIP tokens!\n"
                        f"You can gain more by **voting** with `/vote`\n\n"
                        f"Use your VIP tokens to embed longer videos with Clyppy (up to {EMBED_W_TOKEN_MAX_LEN // 60} minutes!)")
@@ -143,7 +217,166 @@ class Base(Extension):
                             ]
                    )
     async def embed(self, ctx: SlashContext, url: str):
-        return await self.bot.platform_tools.command_embed(ctx, url)
+        async def wait_for_download(clip_id: str, timeout: float = 30):
+            start_time = time.time()
+            while clip_id in self.currently_downloading_for_embed:
+                if time.time() - start_time > timeout:
+                    raise TimeoutError(f"Waiting for clip {clip_id} download timed out")
+                await asyncio.sleep(0.1)
+
+        timeout_task = None
+        await ctx.defer(ephemeral=False)
+
+        if ctx.guild:
+            guild = GuildType(ctx.guild.id, ctx.guild.name, False)
+            ctx_link = f"https://discord.com/channels/{ctx.guild.id}/{ctx.channel.id}"
+        else:
+            guild = GuildType(ctx.author.id, ctx.author.username, True)
+            ctx_link = f"https://discord.com/channels/@me/{ctx.bot.user.id}"
+
+        slug, p = None, None
+        try:
+            if not url.startswith("https://"):
+                url = "https://" + url
+            platform, slug = compute_platform(url, self.bot)
+
+            p = platform.platform_name if platform is not None else None
+            self.logger.info(f"/embed in {guild.name} {url} -> {p}, {slug}")
+
+            if guild.is_dm:
+                nsfw_enabed = True
+            elif isinstance(ctx.channel, TYPE_THREAD_CHANNEL):
+                # GuildPublicThread has no attribute nsfw
+                nsfw_enabed = False
+            else:
+                nsfw_enabed = ctx.channel.nsfw
+
+            if platform is None:
+                self.logger.info(f"return incompatible for /embed {url}")
+                await ctx.send(f"Couldn't embed that url (invalid/incompatible) {create_nexus_str()}")
+                await send_webhook(
+                    title=f'{["DM" if guild.is_dm else guild.name]} - /embed called - Failure',
+                    load=f"user - {ctx.user.username}\n"
+                         f"cmd - /embed url:{url}\n"
+                         f"platform: {p}\n"
+                         f"slug: {slug}\n"
+                         f"response - Incompatible",
+                    color=COLOR_RED,
+                    url=APPUSE_LOG_WEBHOOK
+                )
+                return
+            elif platform.is_nsfw and not nsfw_enabed:
+                await ctx.send(f"This platform is not allowed in this channel. You can either:\n"
+                               f" - If you're a server admin, go to `Edit Channel > Overview` and toggle `Age-Restricted Channel`\n"
+                               f" - If you're not an admin, you can invite me to one of your servers, and then create a new age-restricted channel there\n"
+                               f"\n**Note** for iOS users, due to the Apple Store's rules, you may need to access [discord.com]({ctx_link}) in your phone's browser to enable this.\n")
+                await send_webhook(
+                    title=f'{["DM" if guild.is_dm else guild.name]} - /embed called - Failure',
+                    load=f"user - {ctx.user.username}\n"
+                         f"cmd - /embed url:{url}\n"
+                         f"platform: {p}\n"
+                         f"slug: {slug}\n"
+                         f"response - NSFW disabled",
+                    color=COLOR_RED,
+                    url=APPUSE_LOG_WEBHOOK
+                )
+                return
+
+            if ctx.user.id in self.currently_embedding_users:
+                await ctx.send(f"You're already embedding a video. Please wait for it to finish before trying again.")
+                await send_webhook(
+                    title=f'{["DM" if guild.is_dm else guild.name]} - /embed called - Failure',
+                    load=f"user - {ctx.user.username}\n"
+                         f"cmd - /embed url:{url}\n"
+                         f"platform: {p}\n"
+                         f"slug: {slug}\n"
+                         f"response - Already embedding",
+                    color=COLOR_RED,
+                )
+                return
+            else:
+                self.currently_embedding_users.append(ctx.user.id)
+
+            if slug in self.currently_downloading_for_embed:
+                try:
+                    # if its already downloading from another embed command running at the same time
+                    await wait_for_download(slug, timeout=platform.dl_timeout_secs)
+                except TimeoutError:
+                    pass  # continue with the dl anyway
+            else:
+                self.currently_downloading_for_embed.append(slug)
+
+            timeout_task = asyncio.create_task(self._handle_timeout(ctx, url, platform.dl_timeout_secs))
+            e = AutoEmbedder(self.bot, platform, logging.getLogger(__name__))
+        except Exception as e:
+            if timeout_task is not None:
+                timeout_task.cancel()
+            self.logger.info(f"Exception in /embed: {str(e)}")
+            await ctx.send(f"Unexpected error while trying to embed this url {create_nexus_str()}")
+            await send_webhook(
+                title=f'{["DM" if guild.is_dm else guild.name]} - /embed called - Failure',
+                load=f"user - {ctx.user.username}\n"
+                     f"cmd - /embed url:{url}\n"
+                     f"platform: {p}\n"
+                     f"slug: {slug}\n"
+                     f"response - Unexpected error",
+                color=COLOR_RED,
+                url=APPUSE_LOG_WEBHOOK
+            )
+            return
+
+        success, response = False, "Unknown error"
+        try:
+            await e._process_this_clip_link(
+                clip_link=url,
+                respond_to=ctx,
+                guild=guild,
+                is_embed_text_command=False,
+                try_send_files=True
+            )
+            success, response = True, "Success"
+        except NoDuration:
+            await ctx.send(f"Couldn't embed that url (not a video post) {create_nexus_str()}")
+            success, response = False, "No duration"
+        except NoPermsToView:
+            await ctx.send(f"Couldn't embed that url (no permissions to view) {create_nexus_str()}")
+            success, response = False, "No permisions"
+        except VideoTooLong:
+            if await self._fetch_tokens(ctx.user) >= EMBED_TOKEN_COST:
+                await ctx.send(f"This video was too long to embed (longer than {MAX_VIDEO_LEN_SEC / 60} minutes)\n"
+                               f"It's also longer than {EMBED_W_TOKEN_MAX_LEN // 60} minutes, so using your VIP tokens wouldn't work either...")
+            else:
+                await ctx.send(f"This video was too long to embed (longer than {MAX_VIDEO_LEN_SEC / 60} minutes)\n"
+                               f"Voting with `/vote` will increase it to {EMBED_W_TOKEN_MAX_LEN // 60} minutes! {create_nexus_str()}")
+            success, response = False, "Video too long"
+        except ClipFailure:
+            await ctx.send(f"Unexpected error while trying to download this clip {create_nexus_str()}")
+            success, response = False, "Clip failure"
+        except Exception as e:
+            self.logger.info(f'Unexpected error in /embed: {str(e)}')
+            await ctx.send(f"An unexpected error occurred with your input `{url}` {create_nexus_str()}")
+            success, response = False, "Unexpected error"
+        finally:
+            timeout_task.cancel()
+
+            await send_webhook(
+                title=f'{["DM" if guild.is_dm else guild.name]} - /embed called - {["Success" if success else "Failure"]}',
+                load=f"user - {ctx.user.username}\n"
+                     f"cmd - /embed url:{url}\n"
+                     f"platform: {p}\n"
+                     f"slug: {slug}\n"
+                     f"response - {response}",
+                color=[COLOR_GREEN if success else COLOR_RED],
+                url=APPUSE_LOG_WEBHOOK
+            )
+            try:
+                self.currently_downloading_for_embed.remove(slug)
+            except ValueError:
+                pass
+            try:
+                self.currently_embedding_users.remove(ctx.user.id)
+            except ValueError:
+                pass
 
     #@slash_command(name="alerts", description="Configure Clyppy Alerts (Live Notifications, Video Uploads, etc.")
     #async def alerts(self, ctx: SlashContext):
