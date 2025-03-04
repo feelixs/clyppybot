88fa121397b11e48e74c319b1067c1c3462e41bc
Michael Felix <mikemh@uri.edu>
2025-03-04 18:53:58 -0500

    Update classes.py

--- bot/classes.py
+++ bot/classes.py
@@ -591,11 +591,13 @@ class BaseAutoEmbed:
                 slug=self.platform.parse_clip_url(event.message.content.split(" ")[-1])
             )
         elif self.platform is None:  # this means we're in the 'base' instance, meaning it's the one who will process misc cmds
+            self.logger.info(f"Checking for misc commands...")
             # try the other commands (ones with no params)
             for txt_command, func in self.OTHER_TXT_COMMANDS.items():
                 if event.message.content.strip() == txt_command:
                     return await func(event.message)
         elif self.platform.is_dl_server(event.message.guild) or self.always_embed_this_platform:
+            self.logger.info(f"Checking for embeds...")
             # wasn't a command, maybe it's a link?
             await self.embedder.on_message_create(event)
 
