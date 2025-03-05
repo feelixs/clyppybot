03401121bf25252402250bb6f080e122eb601b47
Michael Felix <mikemh@uri.edu>
2025-03-04 19:35:26 -0500

    Update base.py

--- cogs/base.py
+++ cogs/base.py
@@ -117,7 +117,7 @@ class Base(Extension):
             if len(split) <= 1:
                 return await event.message.reply("Please provide a URL to embed like `.embed https://example.com`")
             else:
-                # handle case where a cog doesnt capture it and take over error handling (platform is None)
+                # handle case where a cog doesn't capture it and take over error handling (platform is None)
                 platform, _ = compute_platform(split[1], self.bot)
                 if platform is None:
                     return await event.message.reply("Sorry, I can't embed that link (incompatible platform)")
