7eafcf2636eb1c12a4db08c800f40403e11a7d4e
Michael Felix <myellahouse@gmail.com>
2025-03-14 07:52:54 -0400

    Update tiktok.py

--- bot/platforms/tiktok.py
+++ bot/platforms/tiktok.py
@@ -47,7 +47,7 @@ class TikTokMisc(BaseMisc):
             self.logger.info(f'Retrieving actual url from shortened url {url}')
             async with ClientSession() as session:
                 async with session.get(url) as response:
-                    p = r'"canonical":"https:\\u002F\\u002Fwww\.tiktok\.com\\u002F@([\w.]+)\\u002Fvideo\\u002F(\d+)"'
+                    p = r'"canonical":"https:\\u002F\\u002Fwww\.tiktok\.com\\u002F@([\w.]+)\\u002F(?:video|photo)\\u002F(\d+)"'
                     txt = await response.text()
                     video_id, user = re.search(p, txt).group(2), re.search(p, txt).group(1)
                     if user is None:
