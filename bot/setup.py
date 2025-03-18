2185cfd0bc5f6f531ab076d00a84fa49953103d9
Michael Felix <myellahouse@gmail.com>
2025-03-17 23:50:38 -0400

    Update setup.py

--- bot/setup.py
+++ bot/setup.py
@@ -39,9 +39,9 @@ class BaseAutoEmbedForConsistency(BaseAutoEmbed):
     def __init__(self, bot):
         self.bot = bot
         self.logger = logging.getLogger(self.__class__.__name__)
-        self.platform = None
+        self.platform = BASIC_MISC(bot)
         super().__init__(
-            platform=BASIC_MISC(bot),
+            platform=self.platform,
             always_embed=False
         )
 
