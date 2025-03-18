b7863aeedc3c34d48d24e595424267b1265b936d
Michael Felix <myellahouse@gmail.com>
2025-03-18 19:54:42 -0400

    Update base.py

--- bot/platforms/base.py
+++ bot/platforms/base.py
@@ -26,10 +26,8 @@ class BASIC_MISC(BaseMisc):
     def parse_clip_url(self, url: str, extended_url_formats=False):
         parse = urlparse(url)
         netloc = parse.netloc.lower()
-        domain = netloc.split(".")[0]
-
-        self.is_nsfw = any(trigger in domain for trigger in NSFW_DOMAIN_TRIGGERS)
-        if domain in EXTRA_YT_DLP_SUPPORTED_NSFW_DOMAINS:
+        self.is_nsfw = any(trigger in netloc for trigger in NSFW_DOMAIN_TRIGGERS)
+        if netloc in EXTRA_YT_DLP_SUPPORTED_NSFW_DOMAINS:
             self.is_nsfw = True
 
         return url
