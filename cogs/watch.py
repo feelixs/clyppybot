615259caad4f5b9c37ee90a77aac3757ea9b9fdd
Michael Felix <myellahouse@gmail.com>
2025-03-27 16:52:07 -0400

    Update watch.py

--- cogs/watch.py
+++ cogs/watch.py
@@ -3,6 +3,7 @@ from interactions import Extension, listen
 from interactions.api.events import MessageCreate
 import logging
 import asyncio
+from bot.io import add_reqqed_by
 import re
 
 
@@ -99,6 +100,7 @@ class Watch(Extension):
                                         # Handle DM channel case
                                         userid = chnid[1:]  # Remove the 'd' prefix to get the user ID
                                         user = await self.bot.fetch_user(userid)
+                                        self.logger.info(f"Fetching dm with user {user.username}")
                                         dm_channel = await user.fetch_dm(force=True)
                                         msg = await dm_channel.fetch_message(msgid)
                                     else:
