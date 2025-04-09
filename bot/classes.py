b343825798c020486005f2769a2b970f7a2ef424
Michael Felix <myellahouse@gmail.com>
2025-04-08 23:10:27 -0400

    Update classes.py

--- bot/classes.py
+++ bot/classes.py
@@ -975,7 +975,7 @@ class BaseAutoEmbed:
             if dur >= EMBED_TOTAL_MAX_LENGTH:
                 await ctx.send(f"{get_random_face()} I can't embed videos longer than {EMBED_TOTAL_MAX_LENGTH // (60 * 60)} hours total, even with Clyppy VIP Tokens.")
             elif 0 < user_tokens < (video_cost := get_token_cost(dur)):
-                # the user has tokens available & the video can theoretically be embedded with tokens, but the embed still reported too long
+                # the user has tokens available & the video_dur says it can be embedded with tokens, but the embed still reported too long
                 await ctx.send(f"{get_random_face()} This video was too long to embed ({dur / 60:.1f} minutes)\n\n"
                                f"You can normally use {pre}embed on videos under {MAX_VIDEO_LEN_SEC / 60} minutes, but "
                                f"every {EMBED_TOKEN_COST} token can add {MAX_VIDEO_LEN_SEC / 60} minutes of video time.\n"
