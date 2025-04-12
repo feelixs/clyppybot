1cdb01ad3ac2192ef00bd7e374a706d8e1e46ebb
Michael Felix <myellahouse@gmail.com>
2025-04-12 19:03:58 -0400

    Update classes.py

--- bot/classes.py
+++ bot/classes.py
@@ -986,7 +986,7 @@ class BaseAutoEmbed:
                                components=comp)
             else:
                 await ctx.send(content=f"{get_random_face()} This video was too long to embed (longer than {MAX_VIDEO_LEN_SEC / 60} minutes)\n"
-                                       f"Voting with `/vote` will increase it to {EMBED_TOTAL_MAX_LENGTH // 60} minutes!")
+                                       f"Voting with `/vote` will increase it to {EMBED_TOTAL_MAX_LENGTH // 60} minutes per vote!")
             success, response = False, "Video too long"
         except ClipFailure:
             await ctx.send(f"Unexpected error while trying to download this clip {create_nexus_str()}")
