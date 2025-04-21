5abba27811f12a4ee55e1b0e1d06768d86f87b7c
Michael Felix <myellahouse@gmail.com>
2025-04-21 13:53:51 -0400

    Update facebook.py

--- bot/platforms/facebook.py
+++ bot/platforms/facebook.py
@@ -13,26 +13,27 @@ class FacebookMisc(BaseMisc):
     def parse_clip_url(self, url: str, extended_url_formats=False) -> Optional[str]:
         pattern = r'(?:https?://)?(?:www\.)?facebook\.com/([a-zA-Z0-9_-]+)(?:/|$|\?)'
         match = re.match(pattern, url)
-        return match.group(1) if match else None
+        #return match.group(1) if match else None
+        # incompatible because facebook easily detects bot behavior -> limits this IP -> could lead to other issues with other platforms?
+        return None
 
-    async def get_clip(self, url: str, extended_url_formats=False, basemsg=None, cookies=False) -> Optional['FacebookClip']:
-        return None  # incompatible because facebook easily detects bot behavior -> limits this IP -> could lead to other issues with other platforms?
-        #shortcode = self.parse_clip_url(url)
-        #if not shortcode:
-        #    self.logger.info(f"Invalid Facebook URL: {url}")
-        #    raise NoDuration
+    async def get_clip(self, url: str, extended_url_formats=False, basemsg=None, cookies=False) -> 'FacebookClip':
+        shortcode = self.parse_clip_url(url)
+        if not shortcode:
+            self.logger.info(f"Invalid Facebook URL: {url}")
+            raise NoDuration
 
-        #valid, tokens_used, duration = await self.is_shortform(
-        #    url=url,
-        #    basemsg=basemsg,
-        #    cookies=cookies
-        #)
-        #if not valid:
-        #    self.logger.info(f"{url} is_shortform=False")
-        #    raise VideoTooLong(duration)
-        #self.logger.info(f"{url} is_shortform=True")
+        valid, tokens_used, duration = await self.is_shortform(
+            url=url,
+            basemsg=basemsg,
+            cookies=cookies
+        )
+        if not valid:
+            self.logger.info(f"{url} is_shortform=False")
+            raise VideoTooLong(duration)
+        self.logger.info(f"{url} is_shortform=True")
 
-        #return FacebookClip(shortcode, self.cdn_client, tokens_used, duration)
+        return FacebookClip(shortcode, self.cdn_client, tokens_used, duration)
 
 
 class FacebookClip(BaseClip):
